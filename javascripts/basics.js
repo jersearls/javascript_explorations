7b573a6f90d6c7346216b9a183cfc2cb655b5ea2
Jeremy <searls@me.com>
2017-05-09 17:38:03 -0500

    add comments to code

--- javascripts/basics.js
+++ javascripts/basics.js
@@ -1,16 +1,18 @@
+// simple first function
 function welcome() {
 
-  var greeting = "I'm so glad to be learning a new language, and sharing it dynamically!";
+  var greeting = "I'm so glad to be learning a new language, and sharing it dynamically with you!";
   $("#main").append( greeting );
 }
 
+// use prompt method
 user_name = prompt("What's your name?");
 
+// use string interpolation with prompt variable
 function address(name){
   $("#main").append("Hi " + name + "! it's really nice to meet you. " );
 }
 
+// call address function with user_name as an argument
 address(user_name);
-
 welcome();
-
